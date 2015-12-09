09ba69c9875adfdbf36edaf61287d2b49446ca44
Ian Behrmann <ianjbehrmann@gmail.com>
2015-12-09 13:50:19 -0700

    initial commit with basic greeting route

---
+++ ChartJournal/src/main/java/com/example/ChartJournalApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChartJournalApplication.class, args);
+    }
+}
