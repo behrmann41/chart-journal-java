11784f1b7e449d24e7c74e7877003aaa735f7978
Ian Behrmann <ianjbehrmann@gmail.com>
2015-12-10 13:10:01 -0700

    Added secondary route - working on first view template

--- ChartJournal/src/main/java/com/example/Greeting.java
+++ ChartJournal/src/main/java/com/example/Greeting.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,5 +11,15 @@ public class Greeting {
 	@RequestMapping("/greeting")
 	public @ResponseBody String greeting(){
 		return "hello";
+	}
+	
+	@RequestMapping("/test")
+	public @ResponseBody String test(){
+		return "test";
+	}
+	
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String homepage(){
+		return "templates/home";
 	}
 }
